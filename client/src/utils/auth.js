0e028817a12e12ef9e4bf1e6ba30884e7590642d
MDemp <mdempsey.cbc.ut@gmail.com>
2022-03-16 17:37:16 -0500

    Defining method logged in , expireed token.

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -13,5 +13,25 @@ class AuthService {
      localStorage.setItem("user_token", userToken)
      window.location.assign("/") //If user log in successful, direct to homepage
  }  
+ logout(){
+     localStorage.removeItem("user_token")
+     window.location.assign("/") // If user logs out, reomve their token.
+ }
+ loggedIn(){
+     const currentuserToken = this.getuserToken()
+     return currentuserToken && !this.isTokenExpired(currentuserToken) ? true : false;
+ }
+
+ isTokenExpired(token) {
+    // Decode the token to get its expiration time that was set by the server
+    const decoded = decode(token);
+    // If the expiration time is less than the current time (in seconds), the token is expired and we return `true`
+    if (decoded.exp < Date.now() / 1000) {
+      localStorage.removeItem('user_token');
+      return true;
+    }
+    // If token hasn't passed its expiration time, return `false`
+    return false;
+  }
 }
 export default new AuthService()
