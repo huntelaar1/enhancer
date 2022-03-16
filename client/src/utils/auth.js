d513c5308f8b7a292c8a5bd0b87e8a24dab43b86
MDemp <mdempsey.cbc.ut@gmail.com>
2022-03-16 17:12:23 -0500

    Created login method which sets userToken in localStorage.

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,12 +1,17 @@
 import decode from "jwt-decode"
 
 class AuthService {
-    //Get from local storage depend on what Hunter rename the getItem
+    //Get from local storage depend on what name is assigned in the setItem
     getuserToken(){
-        return localStorage.getItem("id_token")
+        return localStorage.getItem("user_token")
     }
     getuserProfile (){
         return decode(this.getuserToken())
     }
+ //Need methods for login, logout, logged in, expired token 
+ login(userToken){
+     localStorage.setItem("user_token", userToken)
+     window.location.assign("/") //If user log in successful, direct to homepage
+ }  
 }
 export default new AuthService()
