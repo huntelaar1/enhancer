4dd2dec910f718a12493d2240670eb60522d7936
huntelaar1 <95830548+huntelaar1@users.noreply.github.com>
2022-03-24 13:01:06 -0700

    Update SavedArt.js

--- client/src/pages/SavedArt.js
+++ client/src/pages/SavedArt.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {
-Jumbotron,
-Container,
-CardColumns,
-Card,
-Button,
+  Jumbotron,
+  Container,
+  CardColumns,
+  Card,
+  Button,
 } from 'react-bootstrap';
 
 import { useQuery, useMutation } from '@apollo/client';
@@ -15,86 +15,80 @@ import { removeArtId } from '../utils/localStorage';
 import Auth from '../utils/auth';
 
 const SavedArt = () => {
-const { loading, data } = useQuery(QUERY_ME);
-const [removeArt, { error }] = useMutation(REMOVE_ART);
+  const { loading, data } = useQuery(QUERY_ME);
+  const [removeArt, { error }] = useMutation(REMOVE_ART);
 
-const userData = data?.me || {};
+  const userData = data?.me || {};
 
-// create function that accepts the art's mongo _id value as param and deletes the art from the database
-const handleDeleteArt = async (artId) => {
-// get token
-const token = Auth.loggedIn() ? Auth.getToken() : null;
+  // create function that accepts the art's mongo _id value as param and deletes the art from the database
+  const handleDeleteArt = async (artId) => {
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-if (!token) {
-  return false;
-}
+    if (!token) {
+      return false;
+    }
 
-try {
-  const { data } = await removeArt({
-    variables: { artId },
-  });
+    try {
+      const { data } = await removeArt({
+        variables: { artId },
+      });
 
-  // upon success, remove art's id from localStorage
-  removeArtId(artId);
-} catch (err) {
-  console.error(err);
-}
+      // upon success, remove art's id from localStorage
+      removeArtId(artId);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+
+  return (
+    <>
+      <Jumbotron fluid className="text-light bg-dark">
+        <Container>
+          <h1>Viewing {userData.username}'s Art!</h1>
+        </Container>
+      </Jumbotron>
+      <Container>
+        <h2>
+          {userData.savedArt?.length
+            ? `Viewing ${userData.savedArt.length} saved ${
+                userData.savedArt.length === 1 ? 'art' : 'art'
+              }:`
+            : 'You have no saved art!'}
+        </h2>
+        <CardColumns>
+          {userData.savedArt?.map((art) => {
+            return (
+              <Card key={art.artId} border="dark">
+                {art.image ? (
+                  <Card.Img
+                    src={art.image}
+                    alt={`The artwork ${art.title}`}
+                    variant="top"
+                  />
+                ) : null}
+                <Card.Body>
+                  <Card.Title>{art.title}</Card.Title>
+                  <p className="small">Artists: {art.artists}</p>
+                  <Card.Text>{art.description}</Card.Text>
+                  <Button
+                    className="btn-block btn-danger"
+                    onClick={() => handleDeleteArt(art.artId)}
+                  >
+                    Delete this Art!
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </CardColumns>
+      </Container>
+    </>
+  );
 };
 
-if (loading) {
-return
-
-LOADING...
-
-;
-}
-return (
-<>
-
-
-
-Viewing {userData.username}'s Art!
-
-
-
-
-
-
-{userData.savedArt?.length
-? Viewing ${userData.savedArt.length} saved ${ userData.savedArt.length === 1 ? 'art' : 'art' }:
-: 'You have no saved art!'}
-
-
-
-{userData.savedArt?.map((art) => {
-return (
-
-{art.image ? (
-<Card.Img
-src={art.image}
-alt={The artwork ${art.title}}
-variant="top"
-/>
-) : null}
-<Card.Body>
-<Card.Title>{art.title}</Card.Title>
-Artists: {art.artists}
-
-
-<Card.Text>{art.description}</Card.Text>
-<Button
-className="btn-block btn-danger"
-onClick={() => handleDeleteArt(art.artId)}
->
-Delete this Art!
-
-</Card.Body>
-
-);
-})}
-
-
-</>
-);
-};
 export default SavedArt;
